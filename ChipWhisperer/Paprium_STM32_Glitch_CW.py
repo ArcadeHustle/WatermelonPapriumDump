6aa552538ed5e5cf516c9e2f2059cdd0e3e066ef
pyro <hostile@me.com>
2021-09-18 02:09:54 -0400

    fix ADC lock fail

--- ChipWhisperer/Paprium_STM32_Glitch_CW.py
+++ ChipWhisperer/Paprium_STM32_Glitch_CW.py
@@ -119,10 +119,9 @@ scope.io.hs2 = "glitch"  # output glitch_out on the clock line
 scope.gain.gain = 45
 scope.adc.samples = 5000
 scope.adc.offset = 0
-scope.adc.basic_mode = "rising_edge"
 scope.clock.clkgen_freq = 7370000
-scope.clock.adc_src = "clkgen_x4"
 scope.trigger.triggers = "tio4"
+scope.clock.adc_src = "clkgen_x4"
 scope.io.tio1 = "serial_rx"
 scope.io.tio2 = "serial_tx"
 
