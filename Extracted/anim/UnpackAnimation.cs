0305483eb54692fa5a0f31594d0adb7034b315e2
ArcadeHustle <hostile@me.com>
2021-10-24 23:36:00 -0400

    debug statements

--- Extracted/anim/UnpackAnimation.cs
+++ Extracted/anim/UnpackAnimation.cs
@@ -20,15 +20,16 @@ namespace UnpackAnimation {
 			bmp.MakeTransparent();
 			Graphics gfx = Graphics.FromImage(bmp);
 
-			uint addr = 0x0;
+			//uint addr = 0x0;
 			//uint ptr = addr & 0xffffff;
 			uint ptr = 0x7e754;
 
 			int count = data[ptr];
-			Console.WriteLine(count);
+			Console.WriteLine("count: {0}", count);
 			ptr +=2;
 
 			int posX=0, posY=0;
+			Console.WriteLine("posX: {0} posY: {1}", posX, posY);
 
 			for (int s = 0; s < count; s++)
 			{
@@ -36,24 +37,33 @@ namespace UnpackAnimation {
 				{
 					posX = 256 + (sbyte)data[ptr];
 					posY = 256 + (sbyte)data[ptr] + 1;
+					Console.WriteLine("posX: {0} posY: {1}", posX, posY);
 				}
 				else
 				{
 					posX += (sbyte)data[ptr];
 					posY += (sbyte)data[ptr + 1];
+					Console.WriteLine("posX: {0} posY: {1}", posX, posY);
 				}
 				int sizeX = ((data[ptr + 2] & 0xf) >> 2) +1;
+				Console.WriteLine("sizeX: {0}", sizeX);
 				int sizeY = (data[ptr + 2] & 0x3) +1;
+				Console.WriteLine("sizeY: {0}", sizeY);
 				int offset = data[ptr+7];
+				Console.WriteLine("offset: {0}", offset);
 				int block = (data[ptr +4] << 8) + data[ptr + 5];
+				Console.WriteLine("block: {0}", block);
 
 				int blkX = (block & 0x1f) << 5;
+				Console.WriteLine("blkX: {0}", blkX);
 				int blkY = block & 0xffe0;
+				Console.WriteLine("blkY: {0}", blkY);
 
 				for (int x = 0; x < sizeX; x++)
 				{
 					for (int y = 0; y < sizeY; y++)
 					{
+						Console.WriteLine("count: {0} {1}", sizeX, sizeY);
 						gfx.DrawImage(blocks, new Rectangle(posX + x * 8, posY + y * 8, 8, 8), blkX + (offset >> 2) * 8, blkY + (offset & 3) * 8, 8, 8, GraphicsUnit.Pixel);					
 						offset++;
 					}
@@ -62,7 +72,7 @@ namespace UnpackAnimation {
 			}
 						
 			gfx.Dispose();
-			bmp.Save("out.png");
+			bmp.Save(string.Format(@"out_0x{0}{1}.png", posX, posY));
 
 		}
 	}
